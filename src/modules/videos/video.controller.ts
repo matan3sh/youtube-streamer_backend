c89c13c673f0fe45d02a5f2d267e4d5699260e59
Matan Shaviro <matan3sh@gmail.com>
2022-07-16 15:19:38 +0300

    Implement functionality of streaming a video from server with chunks

--- src/modules/videos/video.controller.ts
+++ src/modules/videos/video.controller.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from "express";
 import fs from "fs";
 import busboy from "busboy";
+import { StatusCodes } from "http-status-codes";
 import { createVideo, findeVideos, findVideo } from "./video.service";
 import { Video } from "./video.model";
 import { UpdateVideoParams, UpdateVideoBody } from "./video.schema";
-import { StatusCodes } from "http-status-codes";
 
 const MIME_TYPES = ["video/mp4"];
+const CHUNK_SIZE_IN_BYTES = 1000000; // 1mb
 
 function getPath({
   videoId,
@@ -85,4 +86,52 @@ export async function findVideosHandler(req: Request, res: Response) {
   const videos = await findeVideos();
 
   return res.status(StatusCodes.OK).send(videos);
+}
+
+export async function streamVideoHandler(req: Request, res: Response) {
+  const { videoId } = req.params;
+
+  const range = req.headers.range;
+  if (!range) {
+    return res.status(StatusCodes.BAD_REQUEST).send("Invalid range");
+  }
+
+  const video = await findVideo(videoId);
+  if (!video) {
+    return res.status(StatusCodes.NOT_FOUND).send("Video not found");
+  }
+
+  const filePath = getPath({
+    videoId: video.videoId,
+    extension: video.extension,
+  });
+
+  const fileSizeInBytes = fs.statSync(filePath).size;
+
+  const chunkStart = Number(range.replace(/\D/g, ""));
+  const chunkEnd = Math.min(
+    chunkStart + CHUNK_SIZE_IN_BYTES,
+    fileSizeInBytes - 1
+  );
+
+  // where to start reading from
+  const contentLength = chunkEnd - chunkStart + 1;
+
+  const headers = {
+    "Content-Range": `bytes ${chunkStart}-${chunkEnd}/${fileSizeInBytes}`,
+    "Accept-Ranges": "bytes",
+    "Content-length": contentLength,
+    "Content-Type": `video/${video.extension}`,
+    // "Cross-Origin_Resource-Policy": "cross-origin",
+  };
+  res.writeHead(StatusCodes.PARTIAL_CONTENT, headers);
+
+  // Load the video stream into memory
+  const videoStream = fs.createReadStream(filePath, {
+    start: chunkStart,
+    end: chunkEnd,
+  });
+
+  // send stream to browser
+  videoStream.pipe(res);
 }
